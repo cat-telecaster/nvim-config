71776c978dbaed6ba8ad0bc9834cfa166bf46e49
kurtis <kurtis.chan.1@live.com>
2025-03-31 14:54:51 +0900

    feat: experimental copilot changes
    Signed-off-by: kurtis <kurtis.chan.1@live.com>

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,24 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    suggestion = { enabled = true },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      panel = { enabled = false },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = false,
+          accept_word = "<M-w>",
+          accept_line = "<M-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "/",
+        },
+      },
+      filetypes = { markdown = true },
+    },
   },
 }
+
