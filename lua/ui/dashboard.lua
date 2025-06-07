529f23b1f5b2a63fb732f55d6d2d883c459439ef
kurtis <kurtis.chan.1@live.com>
2025-06-07 13:44:02 +0900

    chore: remove example plugin + add copilot disable toggle

--- lua/ui/dashboard.lua
+++ lua/ui/dashboard.lua
@@ -97,5 +97,58 @@ return {
         },
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+          -- Create some toggle mappings
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+          Snacks.toggle.diagnostics():map("<leader>ud")
+          Snacks.toggle.line_number():map("<leader>ul")
+          Snacks.toggle
+            .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+            :map("<leader>uc")
+          Snacks.toggle.treesitter():map("<leader>uT")
+          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+          Snacks.toggle.inlay_hints():map("<leader>uh")
+          Snacks.toggle.indent():map("<leader>ug")
+          Snacks.toggle.dim():map("<leader>uD")
+
+          -- Add Copilot toggle
+          local copilot_exists = pcall(require, "copilot")
+          if copilot_exists then
+            Snacks.toggle({
+              name = "Copilot Completion",
+              color = {
+                enabled = "azure",
+                disabled = "orange",
+              },
+              get = function()
+                return not require("copilot.client").is_disabled()
+              end,
+              set = function(state)
+                if state then
+                  require("copilot.command").enable()
+                else
+                  require("copilot.command").disable()
+                end
+              end,
+            }):map("<leader>at")
+          end
+          --
+        end,
+      })
+    end,
   },
 }
