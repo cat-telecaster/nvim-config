4b0a8afbd339467065b74c4aebffd058bb16fe8f
kurtis <kurtis.chan.1@live.com>
2025-11-04 22:53:09 +0900

    chore: remove copilot blink

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,19 +1,7 @@
 return {
   "saghen/blink.cmp",
   optional = true,
-  dependencies = { "fang2hou/blink-copilot" },
   opts = {
-    sources = {
-      default = { "copilot" },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-copilot",
-          score_offset = 100,
-          async = true,
-        },
-      },
-    },
     keymap = {
       ["<Tab>"] = {
         "snippet_forward",
@@ -21,19 +9,6 @@ return {
           return require("sidekick").nes_jump_or_apply()
         end,
         "fallback",
-      },
-    },
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    enabled = false,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        -- LazyVim Extra disabled it for copilot.lua
-        copilot = { enabled = true },
       },
     },
   },
