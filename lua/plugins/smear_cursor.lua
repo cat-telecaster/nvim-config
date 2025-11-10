01b3b84c148de21a4ad84f4e9a13314a0d7ba1d6
kurtis <kurtis.chan.1@live.com>
2025-11-10 20:14:05 +0900

    fix: smear cursor colour to match kitty

---
+++ lua/plugins/smear_cursor.lua
@@ -0,0 +1,13 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    cursor_color = "#ff0017",
+    stiffness = 0.3,
+    trailing_stiffness = 0.2,
+    damping = 0.5,
+    trailing_exponent = 3,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
+  },
+}
